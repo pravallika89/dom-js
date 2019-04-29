b771d667a426e4936e72e8a6a09d1c43370d74e4
pravallika <pravallikaammineni@github.com>
2019-04-29 16:38:28 -0700

    added para and changed color and fontsize

--- js/review-dom-manipulation-2.js
+++ js/review-dom-manipulation-2.js
@@ -3,6 +3,8 @@ console.log(para2);
 var para2UsingSelector=document.querySelector('#para-2');para2.margin="2px solid black";
 console.log(para2UsingSelector);
 para2.style.color='green';
-para2.style.margin= '2px solid black';
 
-para2.style.fontsize='30px';
+//doubt
+para2.style.margin= '2px';
+
+para2.style.fontSize='30px';
