7e982010f21688e5598aa6c5618dec0eb6be0305
pravallika <pravallikaammineni@github.com>
2019-04-28 23:03:46 -0700

    reviewed para using script file

---
+++ js/review-dom-manipulation-2.js
@@ -0,0 +1,8 @@
+var para2 = document.getElementById('para-2');
+console.log(para2);
+var para2UsingSelector=document.querySelector('#para-2');para2.margin="2px solid black";
+console.log(para2UsingSelector);
+para2.style.color='green';
+para2.style.margin= '2px solid black';
+
+para2.style.fontsize='30px';
